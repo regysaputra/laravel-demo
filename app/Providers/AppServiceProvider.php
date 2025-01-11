705ef21088d30b8aa1742e5011ef50bc267dbe16
Regy Saputra <regysaputra27@gmail.com>
2025-01-11 17:48:32 +0700

    edit vercel.json

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,6 +12,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
+        // If you remove this, css and js cannot load over https
         if (env('APP_ENV') === 'production') {
             URL::forceScheme('https');
         }
